8e54653b6f66b4facf9f1f75d50386793f959139
rithikreddypalla <rithikreddypalla@gmail.com>
2026-06-01 05:18:54 +0530

    Update MAC modules

--- mac.v
+++ mac.v
@@ -8,107 +8,21 @@ module mac(
     output [31:0] result
 );
 
-    wire [7:0] a_reg_out_in_reg, b_reg_out_in_reg;
-    wire setup_out_in_reg;
-
-    input_register in_reg (
+    wire [7:0] partial_product;
+    wire done;
+    wire [31:0] prod_ext;
+    multiply mul (
         .a(a),
         .b(b),
-        .setup(new_data),
         .clk(clk),
         .rst(rst),
-        .a_out(a_reg_out_in_reg),
-        .b_out(b_reg_out_in_reg),
-        .setup_out(setup_out_in_reg)
+        .load(new_data),
+        .product(partial_product),
+        .done(done)
     );
 
+    assign prod_ext = { {24{partial_product[15]}}, partial_product };
 
-
-    wire [7:0] a_comp_in;
-    assign a_comp_in = ~a_reg_out_in_reg; // 1's complement
-    wire [7:0] a_neg;
-
-    full_adder_8bit adder_neg (
-        .a(a_comp_in),
-        .b(8'b0),
-        .carry_in(1'b1), // for 2's complement
-        .sum(a_neg),
-        .carry_out()
-    );
-
-
-
-    wire [7:0] a_reg_out_mul_reg, b_reg_out_mul_reg, a_neg_out_mul_reg;
-    wire setup_out_mul_reg, setup_out_shifted;
-
-    multiplier_register mul_reg (
-        .a(a_reg_out_in_reg),
-        .b(b_reg_out_in_reg),
-        .a_neg(a_neg),
-        .setup(setup_out_in_reg),
-        .clk(clk),
-        .rst(rst),
-        .a_out(a_reg_out_mul_reg),
-        .b_out(b_reg_out_mul_reg),
-        .a_neg_out(a_neg_out_mul_reg),
-        .setup_out(setup_out_mul_reg)
-    );
-
-    // Insert shift register for setup signal
-    shift_register_8bit setup_shift_reg (
-        .in(setup_out_mul_reg),
-        .clk(clk),
-        .rst(rst),
-        .out(setup_out_shifted)
-    );
-
-
-    wire [15:0] product;
-    multiply mul (
-        .a(a_reg_out_mul_reg),
-        .b(b_reg_out_mul_reg),
-        .a_neg(a_neg_out_mul_reg),
-        .clk(clk),
-        .rst(rst),
-        .p_sig_q(setup_out_mul_reg),
-        .s_sig_q(~setup_out_mul_reg),
-        .setup(setup_out_mul_reg),
-        .product(product)
-    );
-
-    wire [31:0] product_ext;
-    assign product_ext = {{16{product[15]}}, product}; // extend to 32 bits
-
-    wire add_sig_out_acc_reg;
-    wire [31:0] product_out_acc_reg;
-
-    accumulator_register acc_reg (
-        .product(product_ext),
-        .add_sig(setup_out_shifted),
-        .clk(clk),
-        .rst(rst),
-        .product_out(product_out_acc_reg),
-        .add_sig_out(add_sig_out_acc_reg)
-    );
-
-    //store accumulate in register
-    wire [31:0] acc_in, acc_out;
-
-    register_32bit_acc acc(
-        .in(acc_in),
-        .add_sig(add_sig_out_acc_reg),
-        .clk(clk),
-        .rst(rst),
-        .out(acc_out)
-    );
     
-    full_adder_32bit adder(
-        .a(acc_out),
-        .b(product_out_acc_reg),
-        .carry_in(1'b0),
-        .sum(acc_in),
-        .carry_out()
-    );
-    assign result = acc_out;
 
 endmodule
